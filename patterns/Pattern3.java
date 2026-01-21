6d6c057e80298279ad124c95b6f23921442e706c
kunalNK7387 <kunalkhairnar595@gmail.com>
2026-01-21 10:44:19 +0530

    Pattern3

--- patterns/Pattern3.java
+++ patterns/Pattern3.java
@@ -2,10 +2,11 @@ public class Pattern3 {
     public static void main(String[] args) {
         int n=3;
         int count=1;
-        for(int i=1;n<=i;i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                System.out.print(i);
+                System.out.print(count);
                 count++;
+                  
             }
             System.out.println("");
         }
