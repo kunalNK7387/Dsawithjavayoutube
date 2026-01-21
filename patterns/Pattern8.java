b096d799c5e3daaa9a6f0dd18f2d0a21ed39b38b
kunalNK7387 <kunalkhairnar595@gmail.com>
2026-01-21 11:29:12 +0530

    PAttern8

---
+++ patterns/Pattern8.java
@@ -0,0 +1,14 @@
+public class Pattern8 {
+    public static void main(String[] args) {
+        int n=3;
+        char count= 'A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(count);
+               
+            }
+            count++;
+            System.out.println("");
+        }
+    }
+}
