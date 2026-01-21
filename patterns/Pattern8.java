5ac89ef67af2135e96b2752fbf7350c563662d7f
kunalNK7387 <kunalkhairnar595@gmail.com>
2026-01-21 11:50:37 +0530

    pattern9

--- patterns/Pattern8.java
+++ patterns/Pattern8.java
@@ -1,13 +1,25 @@
 public class Pattern8 {
     public static void main(String[] args) {
-        int n=3;
+        // int n=3;
+        // char count= 'A';
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print(count);
+               
+        //     }
+        //     count++;
+        //     System.out.println("");
+        // }
+
+
+         int n=3;
         char count= 'A';
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 System.out.print(count);
-               
+               count++;
             }
-            count++;
+            
             System.out.println("");
         }
     }
---
+++ patterns/Pattern9.java
@@ -0,0 +1,16 @@
+public class Pattern9 {
+      public static void main(String[] args) {
+        int n=3;
+        char ch='A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                
+                System.out.print((char)(ch+j-1));
+               
+            }
+           
+            System.out.println("");
+     
+        }   
+    }
+}
