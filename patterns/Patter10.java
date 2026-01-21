e4f50249b6aa32421ba2aa3501bfd09e32b7bd75
kunalNK7387 <kunalkhairnar595@gmail.com>
2026-01-21 11:51:42 +0530

    Pattern10

--- patterns/Patter10.java
+++ patterns/Patter10.java
@@ -1,3 +1,16 @@
 public class Patter10 {
-    
+     public static void main(String[] args) {
+        int n=3;
+        char ch='A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                
+                System.out.print((char)(ch+i+j-2));
+               
+            }
+           
+            System.out.println("");
+     
+        }   
+    }
 }
