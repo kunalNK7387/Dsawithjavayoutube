4f08b029ad8a06583ec32dcf874e69098c7cd290
kunalNK7387 <kunalkhairnar595@gmail.com>
2026-01-21 10:34:02 +0530

    ....

---
+++ patterns/Pattern3.java
@@ -0,0 +1,13 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n=3;
+        int count=1;
+        for(int i=1;n<=i;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(i);
+                count++;
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ patterns/pattern2.java
@@ -0,0 +1,11 @@
+class pattern2 {
+    public static void main(String[] args) {
+        int n=4;
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(n-j+1);
+            }
+        System.out.println("");
+        }
+    }
+}
