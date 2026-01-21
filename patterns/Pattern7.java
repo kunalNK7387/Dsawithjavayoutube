173a2675447cfb1a2ce100c6e326b771a7a6e761
kunalNK7387 <kunalkhairnar595@gmail.com>
2026-01-21 11:23:59 +0530

    Pattern7

---
+++ patterns/Pattern7.java
@@ -0,0 +1,34 @@
+public class Pattern7 {
+     public static void main(String[] args) {
+        int n=5;
+        
+        // for(int i=1;i<=n;i++){
+        //     int num =i;
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(num);
+        //         num++;
+                
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        // for(int i=1;i<=n;i++){
+        //    for(int j=1;j<=i;j++){
+        //         System.out.print(i+j-1);
+               
+                
+        //     }
+        //     System.out.println("");
+        //  }
+
+         for(int i=1;i<=n;i++){
+           for(int j=1;j<=i;j++){
+                System.out.print(i-j+1);
+               
+                
+            }
+            System.out.println("");
+         }
+    }
+}
